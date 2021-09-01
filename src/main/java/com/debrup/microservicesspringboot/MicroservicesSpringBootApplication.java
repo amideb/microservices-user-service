ee39f52140c48de87ec4d042d104c9c65f1ade3b
Debrup Mondal <debrup.official.365@gmail.com>
2021-09-01 11:28:18 +0530

    contact-service connected

--- src/main/java/com/debrup/microservicesspringboot/MicroservicesSpringBootApplication.java
+++ src/main/java/com/debrup/microservicesspringboot/MicroservicesSpringBootApplication.java
@@ -2,12 +2,20 @@ package com.debrup.microservicesspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroservicesSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroservicesSpringBootApplication.class, args);
+    }
+
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
